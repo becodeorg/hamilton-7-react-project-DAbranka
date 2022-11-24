6f8401e870d04888750c21e0065607c47b8d3743
Abranka <davidabranka@gmail.com>
2022-11-24 11:20:10 +0100

    tailwind installed - design in progress

--- 01-weather-app/src/App.jsx
+++ 01-weather-app/src/App.jsx
@@ -1,7 +1,29 @@
+/*
+  - On the homepage the user can enter the city of his/her choice
+  - By clicking on the Submit button or ENTER it will show the weather for the next 5 days
+  - Use the API https://unsplash.com/ to show the photo of the city they put in the form
+  - The app must be responsive and mobile friendly
+*/
+
+
+
 import { useState } from 'react'
+import Forms from "./components/Forms"
+import "./sass/main.scss"
 
 function App() {
-  return
+  return (
+    <>
+    <section className='rounded-lg flex flex-col w-1/2 h-max p-6 bg-blue-500'>
+    <header className=' text-white'>
+      <h1 className='text-3xl text-transform: uppercase font-bold'>My Weather App</h1>
+    </header>
+    <main>
+      <Forms />
+    </main>
+    </section>
+    </>
+  )
 }
 
 export default App
---
+++ 01-weather-app/src/components/Forms.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Forms = () => {
+  return (
+    <form className='flex  flex-col'>
+        <input className='h-10 rounded-lg' type="text" id="insertCity" placeholder="Insert a City"></input>
+        <button className='px-5 py-2 bg-blue-700 text-white rounded-lg self-start mt-5'>Submit</button>
+  </form>)
+}
+
+export default Forms
