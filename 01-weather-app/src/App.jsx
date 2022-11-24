2685a11e0d94cae35a6dc6ef96ba3dbc4397034a
Abranka <davidabranka@gmail.com>
2022-11-24 11:32:05 +0100

    design updated

--- 01-weather-app/src/App.jsx
+++ 01-weather-app/src/App.jsx
@@ -4,9 +4,7 @@
   - Use the API https://unsplash.com/ to show the photo of the city they put in the form
   - The app must be responsive and mobile friendly
 */
-
-
-
+/*------------------------------------------------------------------------- */
 import { useState } from 'react'
 import Forms from "./components/Forms"
 import "./sass/main.scss"
@@ -14,9 +12,9 @@ import "./sass/main.scss"
 function App() {
   return (
     <>
-    <section className='rounded-lg flex flex-col w-1/2 h-max p-6 bg-blue-500'>
+    <section className='rounded-lg flex flex-col h-max p-6 bg-blue-500'>
     <header className=' text-white'>
-      <h1 className='text-3xl text-transform: uppercase font-bold'>My Weather App</h1>
+      <h1 className='text-3xl text-transform: uppercase font-bold p-5'>My Weather App</h1>
     </header>
     <main>
       <Forms />
--- 01-weather-app/src/components/Forms.jsx
+++ 01-weather-app/src/components/Forms.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Forms = () => {
   return (
     <form className='flex  flex-col'>
-        <input className='h-10 rounded-lg' type="text" id="insertCity" placeholder="Insert a City"></input>
+        <input className='h-10 rounded-lg p-2' type="text" id="insertCity" placeholder="Insert a City"></input>
         <button className='px-5 py-2 bg-blue-700 text-white rounded-lg self-start mt-5'>Submit</button>
   </form>)
 }
